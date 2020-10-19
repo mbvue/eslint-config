771fa7367d895b82b6e99f7a43aa6d92dfee7815
hwq <17031989083@qq.com>
2020-10-19 15:11:11 +0800

    初始化格式配置

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/vue",
+  ],
+
+  parserOptions: {
+    ecmaVersion: 11,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+
+  plugins: ["@typescript-eslint"],
+
+  rules: {},
+};
